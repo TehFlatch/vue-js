e8f7442a6a6c7923e8b06e682bb8a5fe18970384
Vladimir <vlachmed@gmail.com>
2021-09-25 01:55:42 +0200

    Init

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <user-item />
+  </div>
+</template>
+
+<script>
+import UserItem from '../components/UserItem.vue';
+import axios from 'axios';
+console.log(axios);
+export default {
+  components: { UserItem },
+  name: 'Dashboard',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
